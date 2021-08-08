ad4407a0f45871df10e699600f0f91bb01c8137b
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-08-09 00:00:51 +0800

    Redo test function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 14:04:53 by jatan             #+#    #+#             */
-/*   Updated: 2021/08/08 21:24:30 by jatan            ###   ########.fr       */
+/*   Updated: 2021/08/08 23:40:49 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,8 @@ int	main(void)
 {
 	int		fd;
 	char	*s;
-	int		i;
 
-	i = 0;
-	fd = open("test", O_RDWR);
-	s = get_next_line(fd);
-	while (s != NULL && i < 10)
-	{
-		printf(">%s", s);
-		free(s);
-		s = get_next_line(fd);
-		i++;
-	}
-	free(s);
+	fd = 0;
+	while ((s = get_next_line(fd)))
+		printf("%s", s);
 }
